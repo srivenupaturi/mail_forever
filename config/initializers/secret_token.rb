6f8dc92a37fdcd2c8340f1c0584f9bdc58648123
srivenupaturi <srivenu_paturi@yahoo.com>
2014-05-10 19:54:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailForever::Application.config.secret_token = '577a196e74e3dee62dcb53b670e8a01bb39aaf5c77de485c7fcaf3d1ae5176fb5e528345b4b12a6160e60a5bc653c9b0af69f0ca875b3d52959eac55ad7a5bac'
