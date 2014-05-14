ecf67fbb16a2f57abca9a2194a27af69e73ba7e2
srivenupaturi <srivenu_paturi@yahoo.com>
2014-05-14 00:36:35 -0400

    added constructors to the models

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,11 @@
 class Recipient < ActiveRecord::Base
   attr_accessible :deleted_at, :disabled_at, :recipient_email, :user_id
   belongs_to :user
+
+  def initialize attributes = {}
+    @user_id = attributes[:user_id]
+    @recipient_email = attributes[:recipient_email]
+    @deleted_at = attributes[:deleted_at]
+    @disabled_at = attributes[:disabled_at]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :hashed_password, :name, :user_name
   has_many :recipients
+
+  def initialize attributes = {}
+    @email = attributes[:email]
+    @hashed_password = processed_password(attributes[:hashed_password])
+    @name = attributes[:name]
+    @user_name = attributes[:user_name]
+  end
+
+  #TODO
+  def processed_password text_password
+     text_password
+  end
 end
